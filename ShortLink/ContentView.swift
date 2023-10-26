511b7a73d5ac94ee2e417df69d08bd8c959e12b6
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-26 10:39:09 -0600

    Initial Commit

---
+++ ShortLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShortLink
+//
+//  Created by Eder  Padilla on 26/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShortLink/ShortLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortLinkApp.swift
+//  ShortLink
+//
+//  Created by Eder  Padilla on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ShortLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
