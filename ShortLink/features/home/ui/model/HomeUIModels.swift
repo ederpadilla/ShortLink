e114e69f743a24c1c352294809aa98ed6c0da2ce
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-26 11:04:47 -0600

    structure pt3

---
+++ ShortLink/features/home/ui/model/HomeUIModels.swift
@@ -0,0 +1,8 @@
+//
+//  HomeUIModels.swift
+//  ShortLink
+//
+//  Created by Eder  Padilla on 26/10/23.
+//
+
+import Foundation
---
+++ ShortLink/features/home/ui/viewmodel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  ShortLink
+//
+//  Created by Eder  Padilla on 26/10/23.
+//
+
+import Foundation
