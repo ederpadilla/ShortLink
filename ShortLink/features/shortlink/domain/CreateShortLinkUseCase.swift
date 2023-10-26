ce93433b9b30fa6fe9d324a0761aa4ac856dfa9d
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-26 17:51:41 -0600

    Add use case

--- ShortLink/features/shortlink/domain/CreateShortLinkUseCase.swift
+++ ShortLink/features/shortlink/domain/CreateShortLinkUseCase.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol CreateShortLinkUseCaseProtocol {
+    func createShortLink(_ url: URL) async throws -> ShortLink
+}
+
+struct CreateShortLinkUseCase: CreateShortLinkUseCaseProtocol {
+    
+    private let shortLinkRepository: ShortLinkRepository
+    
+    init(shortLinkRepository: ShortLinkRepository) {
+        self.shortLinkRepository = shortLinkRepository
+    }
+    
+    func createShortLink(_ url: URL) async throws -> ShortLink {
+        try await shortLinkRepository.createShortLink(url)
+    }
+}
