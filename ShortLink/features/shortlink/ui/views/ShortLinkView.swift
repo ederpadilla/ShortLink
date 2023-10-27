4071987d0bf0676a68a0b4efa870eb47381d3e6b
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-27 15:27:03 -0600

    Fix

--- ShortLink/features/shortlink/ui/views/ShortLinkView.swift
+++ ShortLink/features/shortlink/ui/views/ShortLinkView.swift
@@ -12,6 +12,11 @@ struct ShortLinkView: View {
     @StateObject var viewModel: ShortLinkViewModel
     @State private var texto = ""
     @State private var toast = Toast(isShowing: true, message: "¡Este es un Toast!")
+    let items: [Item] = [
+        Item(name: "John Doe", id: "1234", original: "Original 1"),
+        Item(name: "Jane Smith", id: "5678", original: "Original 2"),
+        // Agrega más elementos aquí
+    ]
     
     var body: some View {
         
@@ -23,12 +28,12 @@ struct ShortLinkView: View {
                     HStack {
                         TextField(String(localized:"Insert URL Shortener"),
                                   text: $texto)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            .padding(.point10)
-                            .background(Color(UIColor.systemGray6))
-                            .cornerRadius(.point10)
-                            .keyboardType(.URL)
-                            .textContentType(.URL)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .padding(.point10)
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(.point10)
+                        .keyboardType(.URL)
+                        .textContentType(.URL)
                         
                         Button(action: {
                             print("Texto enviado: \(texto)")
@@ -48,7 +53,13 @@ struct ShortLinkView: View {
                                         leading: .point16,
                                         bottom: .point0,
                                         trailing: .point16))
-                    Spacer()
+                    
+                    
+                    List(items) { item in
+                        ShortLinkItemView(item: item)
+                    }
+                    .listStyle(PlainListStyle())
+                    
                 }
                 .navigationTitle(String(localized: "Short Link Title"))
                 .overlay { ToastView(toast: $toast) }
@@ -60,8 +71,48 @@ struct ShortLinkView: View {
     func createShortLink() {
         viewModel.createShortLink(url: URL(safeString: "https://www.facebook.com"))
     }
+    
+    
+}
+
+struct Item: Identifiable {
+    var name: String
+    var id: String
+    var original: String
 }
 
 #Preview {
     ShortLinkInjector.provideShortLinkView()
+}
+
+struct ShortLinkItemView: View {
+    
+    let item: Item
+    
+    var body: some View {
+        
+        ZStack {
+            VStack {
+                Text("·\(item.name)")
+                    .font(.system(size: .point16, weight: .bold))
+                    .foregroundColor(.white)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                
+                Text("·\(item.id)")
+                    .font(.system(size: .point14))
+                    .foregroundColor(.gray)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                
+                Text("·\(item.original)")
+                    .font(.system(size: .point14))
+                    .foregroundColor(.gray)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            }
+            .padding(.point10)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: .point10)
+                .stroke(Color(UIColor.systemGray2), lineWidth: .point1)
+        )
+    }
 }
--- ShortLink/foundation/core/CGFloat+Spacing.swift
+++ ShortLink/foundation/core/CGFloat+Spacing.swift
@@ -10,8 +10,11 @@ import Foundation
 extension CGFloat {
     
     static var point0: CGFloat { return 0 }
+    static var point1: CGFloat { return 1 }
     static var point4: CGFloat { return 4 }
+    static var point5: CGFloat { return 5 }
     static var point10: CGFloat { return 10 }
+    static var point14: CGFloat { return 14 }
     static var point16: CGFloat { return 16 }
     static var point24: CGFloat { return 24 }
     static var point32: CGFloat { return 32 }
