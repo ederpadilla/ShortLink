7e62eaf5e0821188e1bdafdfc08b6badafeca956
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-27 12:47:23 -0600

    Add title

--- ShortLink/features/shortlink/ui/views/ShortLinkView.swift
+++ ShortLink/features/shortlink/ui/views/ShortLinkView.swift
@@ -16,39 +16,47 @@ struct ShortLinkView: View {
     var body: some View {
         
         ZStack {
-            VStack {
+            NavigationView {
                 
-                HStack {
-                    TextField("Ingresa tu texto", text: $texto)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .padding(.point10)
-                        .background(Color(UIColor.systemGray6))
-                        .cornerRadius(.point10)
-                        .keyboardType(.URL)
-                        .textContentType(.URL)
+                VStack {
                     
-                    Button(action: {
-                        print("Texto enviado: \(texto)")
-                        hideKeyboard()
-                    }) {
-                        Image(systemName: "paperplane.fill")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.blue)
+                    HStack {
+                        TextField("Ingresa tu texto", text: $texto)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .padding(.point10)
+                            .background(Color(UIColor.systemGray6))
                             .cornerRadius(.point10)
+                            .keyboardType(.URL)
+                            .textContentType(.URL)
+                        
+                        Button(action: {
+                            print("Texto enviado: \(texto)")
+                            hideKeyboard()
+                        }) {
+                            Image(systemName: "paperplane.fill")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(.point10)
+                        }
+                        .frame(maxHeight: .infinity)
                     }
-                    .frame(maxHeight: .infinity)
+                    .frame(maxWidth: .infinity, maxHeight: .point40)
+                    .padding(EdgeInsets(top: .point32,
+                                        leading: .point16,
+                                        bottom: .point0,
+                                        trailing: .point16))
+                    Spacer()
                 }
-                .frame(maxWidth: .infinity, maxHeight: .point40)
-                .padding(EdgeInsets(top: .point45, leading: .point16, bottom: .point0, trailing: .point16))
-                Spacer()
-            }
-            .overlay {
-                ToastView(isPresented: $showToast) {
-                    Text("¡Este es un Toast!")
+                .navigationTitle(String(localized: "Short Link Title"))
+                .overlay {
+                    ToastView(isPresented: $showToast) {
+                        Text("¡Este es un Toast!")
+                    }
                 }
             }
+            
         }
     }
     
--- ShortLink/foundation/core/CGFloat+Spacing.swift
+++ ShortLink/foundation/core/CGFloat+Spacing.swift
@@ -12,6 +12,6 @@ extension CGFloat {
     static var point0: CGFloat { return 0 }
     static var point10: CGFloat { return 10 }
     static var point16: CGFloat { return 16 }
+    static var point32: CGFloat { return 32 }
     static var point40: CGFloat { return 40 }
-    static var point45: CGFloat { return 45 }
 }
